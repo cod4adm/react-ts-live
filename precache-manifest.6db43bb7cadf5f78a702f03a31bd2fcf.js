f88ecead7b0d7ca6122ee2d719cedc1f3c2640c7
cod4adm <57834361+cod4adm@users.noreply.github.com>
2020-07-26 23:53:22 +0300

    Add files via upload

---
+++ precache-manifest.6db43bb7cadf5f78a702f03a31bd2fcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b961f42cc00893b634c23024f1dc01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2d7e4c2ab8ba5ac6b57",
+    "url": "/static/css/main.60c2da9b.chunk.css"
+  },
+  {
+    "revision": "292a6c8a6af090dcd8df",
+    "url": "/static/js/2.eac3a2c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.eac3a2c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2d7e4c2ab8ba5ac6b57",
+    "url": "/static/js/main.fe317672.chunk.js"
+  },
+  {
+    "revision": "d049e954b1c74f29178c",
+    "url": "/static/js/runtime-main.9c7c50c1.js"
+  }
+]);
